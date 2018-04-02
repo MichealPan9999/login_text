bc20672e9f39352150aa8298f74919222c43a5ac
panzq <1174925395@qq.com>
2018-04-02 20:08:09 +0800

    V1.1 添加服务器端代码

---
+++ code_10_login/src/com/pan/login/LoginServlet.java
@@ -0,0 +1,101 @@
+package com.pan.login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public LoginServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// doGet(request, response);
+		// 设置编码为utf-8
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+
+		// 获取用户名和密码
+		String name = request.getParameter("name");
+		String pwd = request.getParameter("pwd");
+		System.out.println("name = " + name + " , pwd = " + pwd);
+		// 校验用户名和密码是否正确
+		if ("admin".equals(name) && "123".equals(pwd)) {// 验证成功
+			HttpSession session = request.getSession();// 获取session
+			session.setAttribute("name", name);// 将用户名和密码保存在session中
+			session.setAttribute("pwd", pwd);// 将用户名和密码保存在session中
+			out.println("success");
+
+			// response.sendRedirect("success.jsp");// 跳转到success.jsp页面
+		} else {// 校验不成功，则留在跳转到login.jsp页面
+			// response.sendRedirect("login.jsp");
+			out.println("fail");
+		}
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to
+	 * post.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 * 
+	 * @throws ServletException
+	 *             if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
